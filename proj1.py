c2dc436f66b01f3e595db231087c0d66b6e2a210
vinigl <vinicius.gl098@gmail.com>
2019-03-28 21:26:22 -0300

    Atualizacao

--- proj1.py
+++ proj1.py
@@ -40,7 +40,7 @@ for incidence in data["*INCIDENCES"]:
     L = math.sqrt((x1 - x0)**2 + (y1 - y0)**2)
     lados.append(L)
     cos = (x1 - x0) / L
-    sin = math.sqrt(1 - (cos**2))   
+    sin = (y1 - y0) / L   
     angs.append([cos,sin])    
 
 matrizes = []
@@ -48,16 +48,15 @@ matrizes = []
 
 for incidence in range(len(data["*INCIDENCES"])):
     ke = []
-    # print(angs[inscidence])
     cos = angs[incidence][0]
     sin = angs[incidence][1]
+
     ke.append([cos**2, cos*sin,-1*cos**2,-1*cos*sin])
     ke.append([cos*sin, sin**2,-1*cos*sin,-1*sin**2])
     ke.append([-1*cos**2,-1*cos*sin,cos**2, cos*sin])
     ke.append([-1*cos*sin,-1*sin**2,cos*sin, sin**2])
     matrizes.append(ke)
 
-# print(matrizes[0])
 
 gl = []
 count = 0
@@ -67,32 +66,95 @@ for i in range(len(data["*COORDINATES"])):
     count += 2
 
 
-matrizes_elemento = []
+matriz_global = []
 
-for incidence in data["*INCIDENCES"]:
-    for i in range(len(data["*ELEMENT_GROUPS"]) * 2):
-        linha=[]
-        for j in range(len(data["*ELEMENT_GROUPS"])* 2):
-            linha.append(0)
-    matrizes_elemento.append(linha)
+for i in range(len(data["*ELEMENT_GROUPS"]) * 2):
+    linha=[]
+    for j in range(len(data["*ELEMENT_GROUPS"])* 2):
+
+        linha.append(0)
+    matriz_global.append(linha)
+
 
 counter = 0
 for i in range(len(data["*ELEMENT_GROUPS"])):
-    m = np.matrix(matrizes[i])
+    m = matrizes[i]
     
     E = 210 *( 10**9)
     A = 2 * (10**-4)
     l = lados[i]
 
-    m = (E*A)/l * m
-    linha = [0,0,0,0,0,0]
+    
     
     gl1 = gl[int(data["*INCIDENCES"][i][1]) - 1]
     gl2 = gl[int(data["*INCIDENCES"][i][2]) - 1]
-    print(gl1,gl2)
-    # linha.insert(gl1/, m)
-    # linha.insert(gl2/,m)
-    # matrizes_elemento.insert()
 
-print(np.matrix(matrizes[0]))
-# print(gl)
+    glT = [gl1[0],gl1[1], gl2[0], gl2[1]]
+    
+
+    for i in range(4):
+        for j in range(4):
+            v = float((E*A)/l) * m[i][j]
+            if (matriz_global[glT[i]][glT[j]] == 0):
+                matriz_global[glT[i]][glT[j]] = v
+            else:
+                matriz_global[glT[i]][glT[j]] = matriz_global[glT[i]][glT[j]] + v
+            
+# print(np.matrix(matriz_global))
+# print(data)
+cc = []
+temp = []
+for i in range(len(data["*COORDINATES"]) *2):
+    temp.append(1)
+
+count = 0
+for i in range(len(data["*COORDINATES"])):
+    for j in range(2):
+        if (int (data["*BCNODES"][i][0]) == i+1):
+            print("rodou")
+            if (int (data["*BCNODES"][i][1]) == 1):
+                temp[i] =  0
+            elif (int (data["*BCNODES"][i][1]) == 2):
+                temp[i+j] = 0
+
+
+# count = 1
+# count_aux = 0
+# for i in range(len(data["*BCNODES"])):
+#     if(count == int( data["*BCNODES"][i][0])):
+#         a = int( data["*BCNODES"][i][1])
+
+#         print("entrou {0}".format(count))
+#         if(a == 1 ):
+#             temp[count-1] = 0
+#         else:
+#             temp[count + count_aux] = 0
+
+
+#     count_aux +1
+#     if count_aux == 2:
+#         count_aux = 0
+#         count +=1
+    
+
+
+
+
+
+#     no = int(node[0])
+#     value = int(node[1])
+
+#     temp[count] = 0
+
+
+print(temp)
+print(data["*BCNODES"])
+
+
+
+
+
+
+
+# print(temp)
+# print(cc)
