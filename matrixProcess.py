4358e1e41d2fa6d6b5c0dadf2d1c19b691ebc8ed
samuel <samuelgranato87@gmail.com>
2019-03-31 22:24:24 -0300

    improved

--- matrixProcess.py
+++ matrixProcess.py
@@ -20,99 +20,118 @@ def matrixProcess(data):
 
     matrizes = []
 
-
-    for incidence in range(len(data["*INCIDENCES"])):
-        ke = []
-        cos = angs[incidence][0]
-        sin = angs[incidence][1]
-
-        ke.append([cos**2, cos*sin, -1*cos**2, -1*cos*sin])
-        ke.append([cos*sin, sin**2, -1*cos*sin, -1*sin**2])
-        ke.append([-1*cos**2, -1*cos*sin, cos**2, cos*sin])
-        ke.append([-1*cos*sin, -1*sin**2, cos*sin, sin**2])
-        matrizes.append(ke)
-
     gl = []
     count = 0
+    # print(np.array(matrizes))
     for i in range(len(data["*COORDINATES"])):
 
         gl.append([count, count + 1])
         count += 2
 
-    temp = []
-    temp2 = []
+
+    matriz_global = np.zeros((len(data["*COORDINATES"])*2,len(data["*COORDINATES"])*2))
+    E_list = []
 
 
-    for i in range(len(data["*COORDINATES"]) * 2):
-        temp.append(1)
+    for i in range(len(data["*INCIDENCES"])):
+        ke = []
+        matriz_rigidez_aux = []
 
-    for i in range(len(data["*COORDINATES"])):
-        temp2.append([1, 1])
+        for k in range(len(data["*COORDINATES"]) * 2):
+            linha = []
+            for l in range(len(data["*COORDINATES"])*2):
+                linha.append(0)
+            matriz_rigidez_aux.append(linha)
+
+        # print(matriz_global)
+
+        # print(ke)
 
 
-    for grau_list in data["*BCNODES"]:
-        temp2[int(grau_list[0])-1][int(grau_list[1])-1] = 0
+        E = float(data["*MATERIALS"][i][0])
+        E_list.append(E)
 
+        A = float(data["*GEOMETRIC_PROPERTIES"][i][0])
+        lado = lados[i]
 
-    index_count = 0
-    glcort = {}
-
-    for i in range(len(temp2)):
-        for j in range(2):
-            index_count += 1
-            if temp2[i][j] == 1:
-                glcort[str(index_count - 1)] = temp2[i].count(1)
-
-    c = 0
-    for i in glcort.values():
-        c += i
-
-    matriz_global = []
-
-    for i in range(c):
-        linha = []
-        for j in range(c):
-
-            linha.append(0)
-        matriz_global.append(linha)
-
-    # print(np.matrix(matrizes[-1]))
-    # print(np.matrix(matriz_global))
-
-    counter = 0
-    for i in range(len(data["*ELEMENT_GROUPS"])):
-        m = matrizes[i]
-
-        # E = float(data["*MATERIALS"][0][0])
-        # A = float(data["*GEOMETRIC_PROPERTIES"][0][0])
-        l = lados[i]
-        E = 21 * 10**5
-        A = 10
+        matriz_rigidez_aux = np.zeros((len(data["*COORDINATES"])*2,len(data["*COORDINATES"])*2))
+        cos = angs[i][0]
+        sin = angs[i][1]
 
         gl1 = gl[int(data["*INCIDENCES"][i][1]) - 1]
         gl2 = gl[int(data["*INCIDENCES"][i][2]) - 1]
 
         glT = [gl1[0], gl1[1], gl2[0], gl2[1]]
-        for i in range(4):
-            for j in range(4):
-                # print(glT[i], glT[j])
-                v = float((E*A)/l) * m[i][j]
-                if (matriz_global[glT[i]][glT[j]] == 0):
-                    matriz_global[glT[i]][glT[j]] = v
-                else:
-                    matriz_global[glT[i]][glT[j]] = matriz_global[glT[i]][glT[j]] + v
 
-    # print(np.matrix(matriz_global))
-    # print(data)
+
+        ke.append([cos**2, cos*sin, -1*cos**2, -1*cos*sin])
+        ke.append([cos*sin, sin**2, -1*cos*sin, -1*sin**2])
+        ke.append([-1*cos**2, -1*cos*sin, cos**2, cos*sin])
+        ke.append([-1*cos*sin, -1*sin**2, cos*sin, sin**2])
+
+
+        # ke = np.append(ke,[cos**2, cos*sin, -1*cos**2, -1*cos*sin])
+        # ke = np.append(ke,[-1*cos**2, -1*cos*sin, cos**2, cos*sin])
+        # ke = np.append(ke,[-1*cos**2, -1*cos*sin, cos**2, cos*sin])
+        # ke = np.append(ke,[-1*cos*sin, -1*sin**2, cos*sin, sin**2])
+
+        # print(ke)
+        for k in range(4):
+            for l in range(4):
+
+                matriz_rigidez_aux[glT[k]][glT[l]] = ke[k][l]
+                # matriz_rigidez_aux[glT[k]][glT[l]] = ke[k][l] * float(float((E*A))/float(lado))
+
+                # matriz_global[glT[i]][glT[j]] += v
+
+
+        matriz_global = np.array(matriz_global) + np.array(matriz_rigidez_aux)
+    # print(np.array(matriz_global))
+
+
+
+
+
+    cc = []
+    temp = []
+    temp2 = []
+
+    # print(np.array(matriz_global))
+
+
+
+    # for i in range(len(data["*COORDINATES"]) * 2):
+    #     temp.append(1)
+
+    for i in range(len(data["*COORDINATES"])):
+        temp2.append([1, 1])
+
+
+    print(data["*BCNODES"])
+    for grau_list in data["*BCNODES"]:
+        # print(grau_list)
+        temp2[int(grau_list[0])-1][int(grau_list[1])-1] = 0
+
+    # print(temp2)
+
+    index_count = 0
+    glcort = []
+
+    for i in range(len(temp2)):
+        for j in range(2):
+            index_count += 1
+            if temp2[i][j] == 1:
+                glcort.append(index_count - 1)
+
     # print(glcort)
     # print(lados)
     # print(temp2)
 
     matrix_calcula = []
-    for i in glcort.keys():
+    for i in range(len(glcort)):
         linha = []
-        for j in glcort.keys():
-            linha.append(matriz_global[int(i)][int(j)])
+        for j in range(len(glcort)):
+            linha.append(matriz_global[glcort[i]][glcort[j]])
 
         matrix_calcula.append(linha)
 
@@ -140,7 +159,68 @@ def matrixProcess(data):
     # print(glcort)
 
     ff = []
-    for i in glcort.keys():
-        ff.append(F[int(i)])
+    for i in range(len(glcort)):
+        ff.append(F[glcort[i]])
 
-    return ff, matrix_calcula, flat_temp2,matriz_global
+    return ff, matrix_calcula, flat_temp2,matriz_global,angs,lados,E_list
+
+
+
+# calcula a deformação dos elementos
+def calcStrain(data,angs,desloc_global,lados):
+    sincos_list =[]
+    sincos_list_aux =[]
+
+    for incidence in range(len(data["*INCIDENCES"])):
+        sincos_list_aux = []
+        cos = angs[incidence][0]
+        sin = angs[incidence][1]
+
+        sincos_list_aux.append(-cos)
+        sincos_list_aux.append(-sin)
+        sincos_list_aux.append(cos)
+        sincos_list_aux.append(sin)
+
+        sincos_list.append(sincos_list_aux)
+    # print(np.array(desloc_global))
+    # print(sincos_list)
+    # print()
+    # print()
+    strain_list = []
+    element_desloc_list = []
+    for incidence in data["*INCIDENCES"]:
+        desloc_aux = []
+        # print((int(incidence[1])-1)*2)
+        # print((int(incidence[1])-1)*2 +1)
+        # print()
+        # print((int(incidence[2])-1)*2)
+        # print((int(incidence[2])-1)*2 +1)
+        # print()
+        desloc_aux.append(desloc_global[(int(incidence[1])-1)*2])
+        desloc_aux.append(desloc_global[(int(incidence[1])-1)*2 +1])
+        desloc_aux.append(desloc_global[(int(incidence[2])-1)*2])
+        desloc_aux.append(desloc_global[(int(incidence[2])-1)*2 +1])
+        # strain_list.append(np.array(desloc_aux))
+        # print(np.array(strain_list))
+        desloc_aux = np.array(desloc_aux)
+        desloc_aux.shape = (len(desloc_aux) , 1)
+        element_desloc_list.append(desloc_aux)
+        # print(desloc_aux)
+        # print(np.array(sincos_list) *  desloc_aux )
+        # print(np.transpose(desloc_aux))
+        # break
+
+    # print(element_desloc_list)
+
+    for i in range(len(data["*INCIDENCES"])):
+        strain_list.append((np.array(sincos_list)[i].dot( element_desloc_list[i]))/lados[i])
+
+    return strain_list
+
+def calcStress(strain_list,E_list):
+    stress_list = []
+    for i in range(len(strain_list)):
+        stress_list.append(strain_list[i] * E_list[i])
+
+    # print(np.array(strain_list))
+    # print(np.array(stress_list))
--- proj1.py
+++ proj1.py
@@ -7,7 +7,11 @@ from matrixProcess import *
 
 data = getInput()
 
-print (data)
+# for datai in data:
+#     print(datai)
+#     print(data[datai])
+#
+#     print()
 
 processed = matrixProcess(data)
 
@@ -17,6 +21,9 @@ ff = processed[0]
 matrix_calcula = processed[1]
 flat_temp2 = processed[2]
 matriz_global = processed[3]
+angs = processed[4]
+lados = processed[5]
+E_list = processed[6]
 
 # print(np.matrix(matrix_calcula))
 # print(np.matrix(matriz_global))
@@ -32,6 +39,7 @@ for i in flat_temp2:
     else:
         desloc_global.append(0)
 
+print()
 print(desloc_global)
 
 
@@ -39,34 +47,6 @@ print(desloc_global)
 
 forcas = np.matrix(matriz_global) * np.transpose(np.matrix(desloc_global))
 
-# print(forcas)
-# vetor_stress = []
 
-# for i in angs:
-#     vetor_stress.append([-i[0],-i[1],i[0],i[1]])
-
-        
- 
-
-# # print(vetor_stress)
-# # print(desloc_global)
-
-# desloc_stress = []
-# for i in range(0,len(desloc_global), 2):
-#     desloc_stress.append([desloc_global[i], desloc_global[i+1]])
-
-    
-
-# desform_espec = []
-# for i in range(len(lados)):
-#     const = 1/lados[i]
-#     vetor = vetor_stress[i]
-#     vetor_desloc_total = []
-#     for j in range(len(desloc_stress))
-#         vetor_desloc = desloc_stress[]
-#         if i+1 < len(lados):
-#             vetor_desloc2 = desloc_stress[i+1]
-#         else: 
-#             vetor_desloc2 = desloc_stress[-1]
-#     print(vetor,vetor_desloc)
-    # desform_espec.append()
+strain_list = calcStrain(data,angs,desloc_global,lados)
+stress_list = calcStress(strain_list,E_list)
